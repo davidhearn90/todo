620af0e91f60146d8ffde2da4cffeedbfb8297d0
David Hearn <davidhearn90@gmail.com>
2015-04-16 08:36:59 -0700

    finished milestone 9

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 		<div class="task-list">
 		<ul>
 			<?php require("includes/connect.php"); ?>
-			
+
 		</ul>
 			
 		</div>
@@ -18,6 +18,20 @@
 	<form class="add-new-task" autocomplete="off">
 		<input type="text" name="new-task" placeholder="Add new item..."/>
 	</form>
+	<script>
+	add_task(); //calling the add task function
+	function add_task(){
+		$(".add-new-task").submit(function(){
+			var new_task = $(".add-new-task input[name=new-task]").val();
+  	if(new_task != ""){
+		$.post("includes/add-task.php", {task: new_task}, function(data){
+   		$(("add-new-task input[name=new-task]").val();
+		$(data).appendTo("task-list ul").hide().fadeIn();
+	});
+    return false;
+	});
+}
+</script>
 </div>
 </body>
 </html>
